46fe21b0056159fb403678f1bae9c23cad34f86e
Smouyk91 <jurajber@gmail.com>
2018-10-17 16:46:41 +0200

    1. verzia

---
+++ Nakladne auto/NakladneAuto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nakladne_auto
+{
+    class NakladneAuto
+    {
+        private static int naklad;
+
+        public void Nalozit(int a)
+        {
+            if (a >= 0 && (a + naklad) <= 3000)
+                naklad += a;
+            else
+                Console.WriteLine("Naklad je pritazky!");
+        }
+
+        public void Vylozit(int b)
+        {
+            if (b >= 0 && (naklad -b) >= 0)
+                naklad -= b;
+            else
+                Console.WriteLine("Nie je mozne vylozit!");
+        }
+
+        public void Zostatok()
+        {
+            Console.WriteLine("Zostatok na aute je: {0}", naklad);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Nakladne auto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nakladne_auto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NakladneAuto nakladiak = new NakladneAuto();
+            nakladiak.Zostatok();
+            nakladiak.Nalozit(10000);
+            nakladiak.Nalozit(500);
+            nakladiak.Vylozit(300);
+            nakladiak.Vylozit(1000);
+            nakladiak.Zostatok();
+        }
+    }
+}
